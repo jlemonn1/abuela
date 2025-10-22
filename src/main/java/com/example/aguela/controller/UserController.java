5114570d8d981012166eb11b938b61302df1adba
jlemonn1 <javierlimon6@gmail.com>
2025-10-23 01:08:15 +0200

    v01

--- src/main/java/com/example/aguela/controller/UserController.java
+++ src/main/java/com/example/aguela/controller/UserController.java
@@ -6,8 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.aguela.dto.UserRequest;
 import com.example.aguela.model.User;
 import com.example.aguela.repository.UserRepository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequestMapping("/api/users")
@@ -22,5 +26,16 @@ public class UserController {
     public List<User> getUsers() {
         return userRepository.findAll();
     }
+
+    @PostMapping
+    public User newUser(@RequestBody UserRequest userRequest) {
+        //TODO: process POST request
+        User newUser = new User();
+        newUser.setColor(userRequest.getColor());
+        newUser.setName(userRequest.getColor());
+        
+        return userRepository.save(newUser);
+    }
+    
 }
 
---
+++ src/main/java/com/example/aguela/dto/UserRequest.java
@@ -0,0 +1,8 @@
+package com.example.aguela.dto;
+import lombok.Data;
+
+@Data
+public class UserRequest {
+    private String name;
+    private String color;
+}
